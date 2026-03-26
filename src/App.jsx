25d28392eef0100cb733821d1f2f9e6a722f61b7
MicheleTodeschini <micheletodeschini01@gmail.com>
2026-03-26 15:05:49 +0100

    add products in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
@@ -9,12 +9,45 @@ function App() {
     return obj
   }
   const [query, setQuery] = useState('')
+  const [products, setProducts] = useState([])
 
-  const url = `http://localhost:3333/products?search=[query]`
+  useEffect(() => {
+
+    async function getProducts() {
+
+
+      try {
+        const data = await fetchJson(`http://localhost:3333/products?search=${query}`)
+        console.log(data);
+
+        setProducts(data)
+      } catch (error) {
+        console.error('Non sono riuscito a recuperare i prodotti')
+      }
+
+    }
+    getProducts()
+  }, [query])
+
 
   return (
     <>
       <h1>Cerca tra i nostri prodotti!</h1>
+
+      <input
+        type='text'
+        placeholder='cerca qui i prodotti'
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+      />
+
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name}
+          </li>
+        ))}
+      </ul>
 
     </>
   )
